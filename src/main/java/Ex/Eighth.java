e0463af8cdbc8d49e290827c589e0ffcd005d3da
svivouk <melnykanastsiia1864@gmail.com>
2026-05-13 13:28:53 +0300

    Update Eighth.java

--- src/main/java/Ex/Eighth.java
+++ src/main/java/Ex/Eighth.java
@@ -1,12 +1,13 @@
 package Ex;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Eighth {
     public static List<String> OrderByLength(List<String> words){
         return words.stream()
-                .sorted((a, b) -> Integer.compare(a.length(), b.length()))
+                .sorted(Comparator.comparingInt(String::length))
                 .collect(Collectors.toList());
 
     }
